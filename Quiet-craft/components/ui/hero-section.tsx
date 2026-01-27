006450d806d378963896c2210bc5bcef3704c1e6
isuzwal <ujjwalgaihre0@gmail.com>
2026-01-27 21:55:32 +0530

    not much work today

--- Quiet-craft/components/ui/hero-section.tsx
+++ Quiet-craft/components/ui/hero-section.tsx
@@ -1,17 +1,22 @@
+'use client';
+
+import Link from 'next/link';
+
 export const HeroSection = () => {
   return (
     <div className="w-full  py-10 ">
       <div className="flex   py-10 justify-center items-center">
-        <div
-          className="inline-flex gap-2 px-4 py-1  group border justify-between items-center duration-300 ease-in-out transition-all text-foreground/70
-           hover:text-foreground/60 hover:bg-neutral-100 dark:text-foreground/70
-         dark:border-neutral-900  dark:hover:bg-neutral-900/70
-         border-neutral-200 shadow-xs rounded-2xl"
+        <Link
+          href="/craft"
+          className="inline-flex cursor-pointer gap-2 group px-4 py-1.5 relative justify-between items-center duration-300 ease-in-out transition-all text-foreground/70 hover:text-foreground/60 hover:bg-neutral-100 dark:text-foreground/70
+          dark:hover:bg-neutral-950/80 shadow-xs rounded-2xl"
         >
-          <span className="text-sm">🎉</span>
-          <span className="text-foreground/70 text-sm">{' | '}</span>
-          <p className="text-sm font-medium  flex  gap-1 items-end ">
-            Craft a your design
+          <span className=" absolute inset-0 rounded-2xl p-px bg-gradient-to-r from-blue-400 via-neutral-200 to-red-400 opacity-100 transition-opacity duration-300" />
+          <span className="absolute inset-[1px] rounded-2xl  bg-white dark:bg-neutral-900  pointer-events-none " />
+          <span className="text-sm z-10">🎉</span>
+          <span className="text-foreground/70 text-sm z-10">{' | '}</span>
+          <p className="text-sm font-medium z-10 flex gap-1 items-end">
+            Explore a Craft
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -22,15 +27,12 @@ export const HeroSection = () => {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className=" group-hover:text-neutral-600 group-hover:dark:text-neutral-100
-              group-hover:translate-x-1 duration-300 ease-in-out transition-all
-              
-              lucide  lucide-chevron-right-icon lucide-chevron-right"
+              className=" group-hover:text-neutral-600 group-hover:dark:text-neutral-100 group-hover:translate-x-1 transition-all duration-300"
             >
               <path d="m9 18 6-6-6-6" />
             </svg>
-          </p>{' '}
-        </div>
+          </p>
+        </Link>
       </div>
     </div>
   );
