20e5430cc00e077da6646c8274c127122b396d14
isuzwal <ujjwalgaihre0@gmail.com>
2026-04-05 19:04:37 +0530

    feat:sidebar bg-color fix

--- Quiet-craft/components/ui/app.sidebar.tsx
+++ Quiet-craft/components/ui/app.sidebar.tsx
@@ -4,10 +4,10 @@ import { Sidebar, SidebarHeader, useSidebar } from './sidebar';
 import { SidebarClose } from 'lucide-react';
 import { ScrollArea } from './scroll-area';
 import { DocsLink } from '@/config/docs';
-
+import { usePathname } from 'next/navigation';
 export function AppSidebar() {
   const { toggleSidebar } = useSidebar();
-
+  const pathname = usePathname();
   return (
     <Sidebar className="dark:bg-black/95  bg-gray-100   border-transparent backdrop-blur-md w-[16rem] z-20">
       <SidebarHeader>
@@ -29,10 +29,9 @@ export function AppSidebar() {
       <ScrollArea className="h-full w-full  px-2 py-2 mt-1.5">
         <div>
           <Link
-            href="/docs "
-            className="text-[14px]  gap-2 w-full  inline-flex font-semibold whitespace-nowrap  font-sans dark:text-neutral-300 dark:hover:text-neutral-200 text-neutral-800 dark:hover:border-neutral-900 dark:hover:bg-neutral-900
-             hover:bg-neutral-200/80 hover:border-neutral-200 hover:text-neutral-600
-             cursor-pointer duration-300 transition-all ease-in-out px-3 py-1 justify-start  items-start rounded-lg"
+            href="/docs"
+            className={`text-[14px] gap-2 w-full inline-flex font-medium whitespace-nowrap font-sans px-3 py-1 rounded-lg transition-all duration-300
+               ${pathname === '/docs' ? 'dark:bg-neutral-800  bg-neutral-200 text-neutral-700 dark:text-white' : 'text-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-900/90 hover:bg-neutral-200 '}`}
           >
             <span>Introduction</span>
           </Link>
@@ -40,15 +39,20 @@ export function AppSidebar() {
         <div className="w-full  mt-4  p-2  font-semibold whitespace-nowrap  dark:text-neutral-300  text-neutral-800">
           <h1 className="text-[14px] font-sans text-start ">Components</h1>
           <div className="flex flex-col gap-2  mt-2 ">
-            {DocsLink.map((link) => (
-              <Link
-                key={link.name}
-                href={link.link}
-                className=" w-full    rounded-lg  hover:dark:bg-neutral-900  hover:dark:border-neutral-900 text-[12px] font-sans  duration-300 ease-in-out transition-all  px-2 py-1.5 hover:bg-neutral-200/60 hover:border-neutral-100 shadow-[12px] dark:text-neutral-400 text-neutral-700 text-left"
-              >
-                {link.name}
-              </Link>
-            ))}
+            {DocsLink.map((link) => {
+              const isActive = pathname === link.link;
+
+              return (
+                <Link
+                  key={link.name}
+                  href={link.link}
+                  className={`w-full rounded-lg text-[12px] font-sans px-2 py-1.5 transition-all duration-300 text-left
+                 ${isActive ? 'dark:bg-neutral-800  bg-neutral-200 text-neutral-700 dark:text-white' : 'text-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-900/90 hover:bg-neutral-200 '}`}
+                >
+                  {link.name}
+                </Link>
+              );
+            })}
           </div>
         </div>
       </ScrollArea>
